1cc452951d3081ff1b1e813ac3df29c2a61b1b19
Qiwen Zhu <qiwzhu@iu.edu>
2024-10-12 09:15:24 -0400

    use node object in group node's structuredDataNodes property

--- src/GroupNode.php
+++ src/GroupNode.php
@@ -18,23 +18,24 @@ class GroupNode extends BaseNode{
     public function addChild(BaseNode $node):void
     {
         // empty
-        $thisNodeArray = $this->getNodeArray();
-        if (is_null($thisNodeArray['structuredDataNodes']['structuredDataNode'])){
-            $this->setValue('structuredDataNodes', ['structuredDataNode' => $node->getNodeArray()]);
+//        $thisNodeArray = $this->getNodeArray();
+        $thisNodeArray = $this;
+        if (is_null($thisNodeArray->structuredDataNodes['structuredDataNode'])){
+            $this->setValue('structuredDataNodes', ['structuredDataNode' => $node]);
             // only 1
-        }elseif(!array_is_list($thisNodeArray['structuredDataNodes']['structuredDataNode'])){
-            $theOneChildNode = $thisNodeArray['structuredDataNodes']['structuredDataNode'];
+        }elseif(!array_is_list((array)$thisNodeArray->structuredDataNodes['structuredDataNode'])){
+            $theOneChildNode = $thisNodeArray->structuredDataNodes['structuredDataNode'];
             $tmpArray = [
                 'structuredDataNode' => [
                     $theOneChildNode,
-                    $node->getNodeArray()
+                    $node
                 ]
             ];
             $this->setValue('structuredDataNodes', $tmpArray);
             // multiple
         }else{
-            $tmpArray = $thisNodeArray['structuredDataNodes'];
-            $tmpArray['structuredDataNode'][] = $node->getNodeArray();
+            $tmpArray = $thisNodeArray->structuredDataNodes;
+            $tmpArray['structuredDataNode'][] = $node;
             $this->setValue('structuredDataNodes', $tmpArray);
         }
     }
@@ -42,5 +43,31 @@ class GroupNode extends BaseNode{
     public function setValueStructuredDataNodes(BaseNode $node):void
     {
         $this->addChild($node);
+    }
+
+
+    /**
+     * override the method so that it recursively convert each child into array
+     * @return array
+     */
+    public function getNodeArray(): array
+    {
+        $result = get_object_vars(...)->__invoke($this);
+
+        if (isset($result['structuredDataNodes']['structuredDataNode'])){
+            $childrenArray = [];
+            if (!is_array($result['structuredDataNodes']['structuredDataNode'])){
+                $result['structuredDataNodes']['structuredDataNode'] = [$result['structuredDataNodes']['structuredDataNode']];
+            }
+
+            foreach ($result['structuredDataNodes']['structuredDataNode'] as $childNode) {
+                $childrenArray[] = $childNode->getNodeArray();
+            }
+
+            $result['structuredDataNodes']['structuredDataNode'] = $childrenArray;
+
+        }
+
+        return $result;
     }
 }
